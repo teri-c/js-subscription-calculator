3298ede08c1dba3e6f43fbfd25b153c95c5d92b8
teri.c <teri.charles23@gmail.com>
2023-10-05 17:30:47 -0400

    Created change event listeners

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    //console.log(subType);
+});
 
+subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    //console.log(subDuration);
+});
