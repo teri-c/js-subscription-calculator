6f6155b770fbd4eeb1cb9fb237084b1e8c63fcdc
teri.c <teri.charles23@gmail.com>
2023-10-05 17:41:21 -0400

    Used a function to calculate cost

--- js/script.js
+++ js/script.js
@@ -13,3 +13,14 @@ subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
     //console.log(subDuration);
 });
+
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5; //for basic plan
+    if (subType === "standard") {
+        monthlyCost = 7;
+    } else if (subType === "premium") {
+        monthlyCost = 10;
+    }
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
